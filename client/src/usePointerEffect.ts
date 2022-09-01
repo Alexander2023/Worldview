9c070f2a83a085d6d6272c6d647c64719611b9ab
Alexander <ana2023@uw.edu>
2022-08-31 21:19:54 -0700

    refactor(pointer effect): move listeners into hook
    listener hook avoids re-registering listeners by decoupling handler
    dependencies from internal hook

--- client/src/usePointerEffect.ts
+++ client/src/usePointerEffect.ts
@@ -1,11 +1,16 @@
-import { RootState, useFrame } from "@react-three/fiber";
-import { useEffect, useState } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useRef, useState } from "react";
 import { Raycaster, Vector2 } from "three";
+import { useEventListener } from 'usehooks-ts'
+
 import { CLICKABLE_OBJECT_NAMES, MAX_CLICKABLE_DIST } from "./constants";
 
-// assumes pointer move events fire faster than delay
-// to prevent premature pointer stop events
-const MOUSE_MOVE_TIMER_DELAY = 300;
+const computePointer = (event: PointerEvent) => {
+  const pointer = new Vector2();
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  return pointer;
+};
 
 /**
  * Custom hook for determining the cursor to use
@@ -14,68 +19,25 @@ const MOUSE_MOVE_TIMER_DELAY = 300;
  * @param isCameraMoving true if camera state is changing, false otherwise
  */
 function usePointerEffect(isCameraMoving: boolean) {
-  const [isPointerMoving, setIsPointerMoving] = useState(false);
+  const canvasRef = useRef(document.getElementById('three-canvas'));
   const [pointer, setPointer] = useState<Vector2 | null>(null);
   const [raycaster] = useState(new Raycaster());
+  const {camera, scene} = useThree();
 
-  useEffect(() => {
-    let mouseMoveTimer: NodeJS.Timeout;
-
-    const computePointer = (event: PointerEvent) => {
-      const pointer = new Vector2();
-      pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-	    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
-      return pointer;
-    };
-
-    const handlePointerOver = (event: PointerEvent) => {
-      setPointer(computePointer(event));
-    };
-
-    const handlePointerMove = (event: PointerEvent) => {
-      setPointer(computePointer(event));
-      setIsPointerMoving(true);
-
-      // prevents timer from going off until pointer stops moving
-      clearTimeout(mouseMoveTimer);
-      mouseMoveTimer = setTimeout(() => setIsPointerMoving(false),
-          MOUSE_MOVE_TIMER_DELAY);
-    };
-
-    const threeCanvasElem = document.getElementById('three-canvas');
-
-    if (threeCanvasElem) {
-      threeCanvasElem.addEventListener('pointerover', handlePointerOver);
-      threeCanvasElem.addEventListener('pointermove', handlePointerMove);
-    } else {
-      console.log('three canvas is null');
-    }
-
-    return () => {
-      if (threeCanvasElem) {
-        threeCanvasElem.removeEventListener('pointerover', handlePointerOver);
-        threeCanvasElem.removeEventListener('pointermove', handlePointerMove);
-      } else {
-        console.log('three canvas is null');
-      }
-    };
-  }, []);
-
-  const shouldUseClickableStyle = (state: RootState) => {
+  const shouldUseClickableStyle = () => {
     if (!pointer) {
       return false;
     }
 
-    raycaster.setFromCamera(pointer, state.camera);
-    const intersections = raycaster.intersectObjects(state.scene.children);
+    raycaster.setFromCamera(pointer, camera);
+    const intersections = raycaster.intersectObjects(scene.children);
 
     if (intersections.length === 0) {
       return false;
     }
 
     const intersection = intersections[0];
-    const xzOrigin = new Vector2(state.camera.position.x,
-        state.camera.position.z);
+    const xzOrigin = new Vector2(camera.position.x, camera.position.z);
     const xzDestination = new Vector2(intersection.point.x,
         intersection.point.z);
 
@@ -83,15 +45,31 @@ function usePointerEffect(isCameraMoving: boolean) {
         xzOrigin.distanceTo(xzDestination) <= MAX_CLICKABLE_DIST;
   };
 
-  useFrame((state) => {
-    if (isCameraMoving || isPointerMoving) {
-      if (shouldUseClickableStyle(state)) {
-        document.body.style.cursor = 'pointer';
-      } else {
-        document.body.style.cursor = 'auto';
-      }
+  const updateCursorStyle = () => {
+    if (shouldUseClickableStyle()) {
+      document.body.style.cursor = 'pointer';
+    } else {
+      document.body.style.cursor = 'auto';
+    }
+  };
+
+  useFrame(() => {
+    if (isCameraMoving) {
+      updateCursorStyle();
     }
   });
+
+  const handlePointerOver = (event: PointerEvent) => {
+    setPointer(computePointer(event));
+  };
+
+  const handlePointerMove = (event: PointerEvent) => {
+    setPointer(computePointer(event));
+    updateCursorStyle();
+  };
+
+  useEventListener('pointerover', handlePointerOver, canvasRef);
+  useEventListener('pointermove', handlePointerMove, canvasRef);
 };
 
 export { usePointerEffect };
